65f410d0b96c572e1b692cdcbbd9022c61bff52d
smwbtech <smwb.tech@gmail.com>
2018-10-17 14:56:12 +0700

    Компонент Login.vue - для авторизации пользователя

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,86 @@
 <template lang="html">
-    <p>Login</p>
+
+    <form action="">
+        <input type="text" class="login" placeholder="Email" v-model="email">
+        <input type="password" name="" id="" class="password" placeholder="Password" v-model="password">
+        <button type="submit" @click.prevent="login">Отправить</button>
+    </form>
+
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+
+    methods: {
+        login() {
+            if(!email || !password) {
+                console.log('Показывает сообщение с ошибкой')
+            }
+            else {
+                this.$http.post('/api/auth/login', {login: this.login, password: this.password})
+                .then( res => {
+
+                })
+                .catch( e => {
+                    console.log(e);
+                });
+            }
+        }
+    }
 }
 </script>
 
 <style lang="css" scoped>
+
+    @import './../assets/css/variables.css';
+
+    form {
+        display: flex;
+        align-items: center;
+        flex-flow: column;
+        padding: 40px;
+        position: relative;
+        width: calc(var(--column) * 12);
+        background-color: #fff;
+        border-radius: 5px;
+        box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.2);
+
+        & > * {
+            display: block;
+            width: 80%;
+            margin-bottom: 40px;
+            text-align: center;
+        }
+
+        & > input {
+            border: none;
+            border-bottom: 2px solid var(--grey);
+            font-size: 1rem;
+        }
+
+        & button {
+            display: block;
+            max-width: 40%;
+            font-size: 1rem;
+            margin: 0 auto;
+            border: none;
+            border-radius: 5px;
+            padding: 10px;
+            background-color: #22b1fc77;
+            color: #fff;
+            transition: background-color .2s ease-in;
+            cursor: pointer;
+
+            &:hover {
+                background-color: #22b1fc;
+            }
+        }
+    }
+
 </style>
